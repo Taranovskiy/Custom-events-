0e94b1f9fa6022f04e23a3244d4cb54d31befc48
Vladimir Taranovskiy <8756136@gmail.com>
2019-01-20 23:35:01 +0300

    add Реализовал ередачу данных от ребёнка к родителю через кастомное событие

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
 const inputComponent = {
-  template: `<input class="input is-small" type="text" />`
+  template: `<input class="input is-small" type="text" :placeholder="placeholder" v-model="input" @keyup.enter="handleKeyUp"/>`,
+
+  props: ['placeholder'],
+
+  data() {
+    return {
+      input: ''
+    }
+  },
+
+  methods: {
+    handleKeyUp() {
+      this.$emit('add-note', {
+        note: this.input,
+        timestamp: new Date().toLocaleString()
+      });
+      this.input = '';
+    }
+  }
+
 }
 
 new Vue({
   el: '#app',
+
   components: {
     'input-component': inputComponent
+  },
+
+  data: {
+    notes: [],
+    timestamps: [],
+    placeholder: 'Название заметки'
+  },
+
+  methods: {
+    addNote(evt) {
+      this.notes.push(evt.note);
+      this.timestamps.push(evt.timestamp);
+      console.log('--->>', 'event', evt);
+    }
   }
+
 })
